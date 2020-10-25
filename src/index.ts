2e5a374e0bbc3361bb4bd41afdd8821f15cc182d
tlantz77 <tlantz@gmail.com>
2020-10-25 17:11:41 -0500

    add linked list class

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
-
+import { LinkedList } from './LinkedList';
 
 const numbersCollection = new NumbersCollection([10, 333, -5, 0]);
 const numberSorter = new Sorter(numbersCollection);
@@ -12,3 +12,12 @@ const charCollection = new CharactersCollection('XaazbqG');
 const charSorter = new Sorter(charCollection);
 charSorter.sort();
 console.log(charCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(0);
+linkedList.add(324);
+const linkedListSorter = new Sorter(linkedList);
+linkedListSorter.sort();
+linkedList.print();
