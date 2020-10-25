abd074f6c8b61b7244016a7250c1f734355b48df
tlantz77 <tlantz@gmail.com>
2020-10-25 16:39:21 -0500

    add numbers collection class

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log("derpage");
+import { Sorter } from './Sorter';
+import { NumbersCollection } from './NumbersCollection';
+
+const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
+const sorter = new Sorter(numbersCollection);
+sorter.sort();
+console.log(numbersCollection.data);
